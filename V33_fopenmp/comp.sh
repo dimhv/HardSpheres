559412c8b8543255e4e211a2c68bcc802e5bec0c
lucmirandaF <138286868+lucmirandaF@users.noreply.github.com>
2024-01-31 19:00:09 +0800

    Delete V33_fopenmp directory

--- V33_fopenmp/comp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-gcc -std=c99  -Ofast -fopenmp 3d_hs_parallel_main.c -lm -o 3d_hs_parallel_main
-# gcc -std=c99  -O3 -fopenmp 3d_hs_parallel_main.c -lm -o 3d_hs_parallel_main
-# gcc -fopenmp 3d_hs_parallel_main.c -lm -o 3d_hs_parallel_main
-# gcc 3d_hs_parallel_main.c -lm -o 3d_hs_parallel_main
-
-
