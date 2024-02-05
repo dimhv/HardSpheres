d4286bc587f2eafb8fc66d37426949fcbc5b7e2b
lucmirandaF <138286868+lucmirandaF@users.noreply.github.com>
2024-02-05 21:03:26 +0800

    Add files via upload

--- V9_SG/3d_SG_parallel.c
+++ V9_SG/3d_SG_parallel.c
@@ -12,6 +12,7 @@
 #define SCALE 2.328306436e-10
 
 
+// static int t_PT = 400;
 static int t_PT = 400;
 int num_neighbours = 6;
 
@@ -84,6 +85,7 @@ int main(int argc, char** argv) {
 
     fclose(fin0);
 
+    fac = 0.5*num_threads*(num_threads-1);
 
 //######################################################################################
 //initial conditions definitions
@@ -97,18 +99,29 @@ int main(int argc, char** argv) {
         samples[ii].time = (Time*)malloc(tobs*t_check * sizeof(Time));
     }
 
+    double* q = (double*)malloc((fac) * sizeof(double));
+    double* chi_plot = (double*)malloc((tobs) * sizeof(double));
+
+
+
     //random number initialization
     srand( (int)time(NULL) );
-//     srand( ???? );
-    for(jj=0; jj< num_threads; ++jj){
+//     srand( 9090911 );
+    for(jj=0; jj< num_threads+1; ++jj){
 
         for (ii=0; ii<100; ++ii) seeds[jj].ux = rand();
         for (ii=0; ii<100; ++ii) seeds[jj].uy = rand();
     }
 
-    srand( 73857 );
-    for (ii=0; ii<100; ++ii) seeds[num_threads].ux = rand();
-    for (ii=0; ii<100; ++ii) seeds[num_threads].uy = rand();
+
+    for(ii=0; ii < N; ++ii)
+        samples[0].particles[ii].x0 = (xor64(num_threads, seeds) >= 0.5) ? 1 : -1;
+
+
+    for(ii=0; ii < num_threads; ++ii)
+        for(jj=0; jj < N; ++jj)
+            samples[ii].particles[jj].x = samples[0].particles[jj].x0;
+
 
     //seting index_list
     for(ii=0; ii< num_threads; ++ii)
@@ -118,16 +131,9 @@ int main(int argc, char** argv) {
         for (jj = 0; jj < N; ++jj)
             samples[ii].particles[jj].J =  (int*)malloc(num_neighbours * sizeof(int));
 
-
-
-    for(ii=0; ii < N; ++ii)
-        samples[0].particles[ii].x0 = (xor64(num_threads, seeds) >= 0.5) ? 1 : -1;
-
-
-    for(ii=0; ii < num_threads; ++ii)
-        for(jj=0; jj < N; ++jj)
-            samples[ii].particles[jj].x = samples[0].particles[ii].x0;
-
+    srand( 73857 );
+    for (ii=0; ii<100; ++ii) seeds[num_threads].ux = rand();
+    for (ii=0; ii<100; ++ii) seeds[num_threads].uy = rand();
 
     for(ii=0; ii<N; ++ii){
 
@@ -142,31 +148,37 @@ int main(int argc, char** argv) {
 
     }
 
-
 //     for(ii=0; ii<N; ++ii)
 //         for(jj=0; jj<num_neighbours; ++jj)
 //             samples[0].particles[ii].J[jj] = 1;
 
+//##################################
+//#initial energy
+
     for(ii=0; ii < num_threads; ++ii)
         energy(ii, samples);
 
+//##################################
+// //#beta input from par.dat
 
-    if(beta0 == betaf)
-        for(ii=0; ii < num_threads; ++ii)
-            samples[ii].beta = beta0;
+//     if(beta0 == betaf)
+//         for(ii=0; ii < num_threads; ++ii)
+//             samples[ii].beta = beta0;
+//
+//     else
+//         linspace(beta0, betaf, num_threads, samples);
+//##################################
 
-    else
-        linspace(beta0, betaf, num_threads, samples);
+//##################################
+// //#beta input from beta_in.dat
+    FILE *fin1;
+    sprintf(file_name,"beta_in.dat");
+    fin1 = fopen(file_name, "r");
 
-//     FILE *fin1;
-//     sprintf(file_name,"beta_in.dat");
-//     fin1 = fopen(file_name, "r");
-//
-//     for(ii=0; ii < num_threads; ++ii)
-//         fscanf(fin1,"%lf\n", &samples[ii].beta);
-//
-//
-//     fclose(fin1);
+    for(ii=0; ii < num_threads; ++ii)
+        fscanf(fin1,"%lf\n", &samples[ii].beta);
+    fclose(fin1);
+//##################################
 
 
     //seting counter
@@ -182,6 +194,11 @@ int main(int argc, char** argv) {
     printf("num_threads = %d\n\n", num_threads);
 
     printf("tobs = %.1e, t_check = %d, t_rlx = %d, t_PT = %d\n", (double)tobs, t_check, t_rlx, t_PT);
+
+    printf("\nx[0]=\n");
+    printf("%d %d %d %d", samples[0].particles[0].x, samples[0].particles[1].x, samples[0].particles[2].x, samples[0].particles[3].x );
+    printf("...\n#############################\n");
+
 
     printf("\nJ=\n");
     for(ii=0; ii< 3; ++ii){
@@ -226,7 +243,7 @@ int main(int argc, char** argv) {
     printf("beta\t\te\t\tindex\t\taccept\n");
     for (ii = 0; ii < num_threads; ii++){
         jj = samples[ii].index_list;
-        printf("%.2lf\t\t%d\t\t%d\t\t%.2g\n", samples[ii].beta, samples[jj].e, samples[ii].index_list, samples[ii].accept_num_PT_bac);
+        printf("%.2lf\t\t%.2e\t\t%d\t\t%.2g\n", samples[ii].beta, (double)samples[jj].e, samples[ii].index_list, samples[ii].accept_num_PT_bac);
     }
 
 
@@ -235,7 +252,7 @@ int main(int argc, char** argv) {
 
     for(ii=0; ii < num_threads; ++ii)
         for(jj=0; jj < N; ++jj)
-            samples[ii].particles[jj].x0 = samples[ii].particles[jj].x;
+            samples[ii].particles[jj].x0 = samples[ii].particles[jj].x; //redefinition x0 (after wait time t_rlx)
 
 
     jj = 0;
@@ -249,21 +266,33 @@ int main(int argc, char** argv) {
 
             for (int ll = thread_id * num_iterations_per_thread; ll < (thread_id + 1) * num_iterations_per_thread && ll < num_threads; ll++){
                 MC(ll, samples, seeds);
-
-//                 samples[ samples[ll].index_list ].time[ii].mag_plot =  mag(ll, samples);
                 samples[ ll ].time[ii].q_plot = q_tcf(ll, samples);
             }
         }
 
-        printScrollingValue((double)ii, (double)tobs);
+//         printScrollingValue((double)ii, (double)tobs);
+//         chi_plot[ii] = chi(q, samples);
 
-//         if(ii%10==0){
-//             MC_PT(num_threads, samples, jj, seeds);
-//
-//             if((jj+1)%t_PT==0)
-//                 optm_T_mod(samples);
-//             jj+=1;
-//         }
+
+        if(ii%10==0){
+            MC_PT(num_threads, samples, jj, seeds);
+
+            if((jj+1)%t_PT==0){
+                optm_T_mod(samples);
+                printScrollingValue((double)ii, (double)tobs);
+            }
+            jj+=1;
+        }
+    }
+
+    #pragma omp parallel
+    {
+
+        int num_iterations_per_thread = (num_threads + (num_cores-1)) / num_cores; // Round up division
+        int thread_id = omp_get_thread_num();
+
+        for (int ll = thread_id * num_iterations_per_thread; ll < (thread_id + 1) * num_iterations_per_thread && ll < num_threads; ll++)
+            MC(ll, samples, seeds);
 
     }
 
@@ -273,53 +302,57 @@ int main(int argc, char** argv) {
 // ##################################
 // #plot
 
-//     FILE *fout2;
-//     sprintf(file_name,"./output/mag_%.2d.dat",label);
-//     fout2 = fopen(file_name, "w");
-//
-//     for (ii = 0; ii < num_threads; ii++){
-//
-//         jj = samples[ii].index_list;
-//         fprintf(fout2,"%g %g\n", 1./samples[ii].beta, samples[jj].time[tobs-1].mag_plot);
-//
-//     }
-//     fclose(fout2);
-
-    FILE *fout3;
-    sprintf(file_name,"./output/q_tcf_%.2d.dat",label);
-    fout3 = fopen(file_name, "w");
-
     double av;
 
-//     samples with same beta value (no replica exchanges)
-    for (ii = 0; ii < tobs; ii++){
+//     FILE *fout2;
+//     sprintf(file_name,"./output/chi_%.2d.dat",label);
+//     fout2 = fopen(file_name, "w");
+//
+//     for (ii = 1; ii < tobs; ii++)
+//         fprintf(fout2,"%d %g\n", ii*t_check, chi_plot[ii]);
+//
+//     fclose(fout2);
 
-        av = 0;
-
-        for (kk = 0; kk < num_threads; kk++)
-            av += samples[ kk ].time[ii].q_plot;
-
-        fprintf(fout3,"%d %g\n", ii, av/(double)num_threads);
-
-    }
-
-    fclose(fout3);
+// ##################################
+// #time autocorrelation function
 
 //     FILE *fout3;
 //     sprintf(file_name,"./output/q_tcf_%.2d.dat",label);
 //     fout3 = fopen(file_name, "w");
 //
-//     for (ii = 0; ii < num_threads; ii++){
 //
 //
-//         for (kk = 0; kk < tobs; kk++)
-//             fprintf(fout3,"%d %g\n", kk*t_check, samples[ ii ].time[kk].q_plot);
+// //     samples with same beta value (no replica exchanges)
+//     for (ii = 0; ii < tobs; ii++){
 //
-// //         fprintf(fout3,"\n");
+//         av = 0;
+//
+//         for (kk = 0; kk < num_threads; kk++)
+//             av += samples[ kk ].time[ii].q_plot;
+//
+//         fprintf(fout3,"%d %g\n", ii, av/(double)num_threads);
 //
 //     }
 //
 //     fclose(fout3);
+
+    FILE *fout3;
+    sprintf(file_name,"./output/q_tcf_%.2d.dat",label);
+    fout3 = fopen(file_name, "w");
+
+    for (ii = 0; ii < num_threads; ii++){
+
+
+        for (kk = 0; kk < tobs; kk++)
+            fprintf(fout3,"%d %g\n", kk*t_check, samples[ ii ].time[kk].q_plot);
+
+//         fprintf(fout3,"\n");
+
+    }
+
+    fclose(fout3);
+
+// ##################################
 
     FILE *fout4;
     sprintf(file_name,"./output/report_%.2d.dat",label);
@@ -327,24 +360,36 @@ int main(int argc, char** argv) {
 
     for (ii = 0; ii < num_threads; ii++){
         jj = samples[ii].index_list;
-        fprintf(fout4,"%.2lf\t\t%d\t\t%d\t\t%.2g\n", samples[ii].beta, samples[jj].e, samples[ii].index_list, samples[ii].accept_num_PT_bac);
+        fprintf(fout4,"%.2lf %d %d %.2g %d %d\n", samples[ii].beta, samples[jj].e, samples[ii].index_list, samples[ii].accept_num_PT_bac, tobs, t_rlx);
     }
 
     fclose(fout4);
 
-    FILE *fout5;
-    sprintf(file_name,"./beta_out_%.2d.dat",label);
-    fout5 = fopen(file_name, "w");
+//     FILE *fout5;
+//     sprintf(file_name,"./output/EA_dist_%.2d.dat",label);
+//     fout5 = fopen(file_name, "w");
+//     for (ii = 0; ii < num_threads; ii++)
+//         for (jj = ii+1; jj < num_threads; jj++)
+//             fprintf(fout5,"%g\n", q_tcf_mod(ii,jj,samples));
+//     fclose(fout5);
 
-    for (ii = 0; ii < num_threads; ii++)
-        fprintf(fout5,"%g\n", samples[ii].beta);
+//     kk = 28;
+//     printf("sample target, beta_index:%d   samples_index:%d,   beta:%g\n", kk, samples[kk].index_list, samples[kk].beta);
+//     FILE *fout5;
+//     sprintf(file_name,"./output/conf_%.2d.dat",label);
+//     fout5 = fopen(file_name, "w");
+//
+//     jj = samples[kk].index_list;
+//     for (ii = 0; ii < N; ii++)
+//         fprintf(fout5,"%d\n", samples[jj].particles[ ii ].x);
+//
+//     fclose(fout5);
 
-    fclose(fout5);
-
+    printf("\n\n");
     printf("beta\t\te\t\tindex\t\taccept\n");
     for (ii = 0; ii < num_threads; ii++){
         jj = samples[ii].index_list;
-        printf("%.2lf\t\t%d\t\t%d\t\t%.2g\n", samples[ii].beta, samples[jj].e, samples[ii].index_list, samples[ii].accept_num_PT_bac);
+        printf("%.2lf\t\t%.2e\t\t%d\t\t%.2g\n", samples[ii].beta, (double)samples[jj].e, samples[ii].index_list, samples[ii].accept_num_PT_bac);
     }
 
     printf("\n\n");
